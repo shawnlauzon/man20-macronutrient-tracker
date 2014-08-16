0e05872a5273f6725192dc2875ede1ff5c5978cd
Shawn Lauzon <shawn.lauzon@gmail.com>
2014-08-16 17:42:36 -0400

    Fix bug with wacky isWorkoutDay
    Storing strings in localStorage hits again!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,17 +4,15 @@ angular.module('man20-macnuttrk.controllers', [])
   $scope.calendar = {
     phase: parseInt(window.localStorage['phase']) || 1,
     weekInPhase: parseInt(window.localStorage['weekInPhase']) || 1,
-    isWorkoutDay: function() {
-      var isWorkoutDay = window.localStorage['isWorkoutDay'];
-      if (isWorkoutDay === undefined) {
-        return true;
-      } else {
-        return isWorkoutDay;
-      }
-    }()
+    isWorkoutDay: (window.localStorage['isWorkoutDay'] === 'true' ? true : false)
   };
-  $scope.phases = [ 1, 2, 3, 4];
-  $scope.weeks = [1, 2, 3, 4];
+ 
+  var stats = User.loadStats();
+  var lbm = User.calculateLBM(stats);
+  var maintenanceCalories = User.maintenanceCalories(stats);
+  
+  $scope.macnuts = Macronutrients.forPhase($scope.calendar, lbm, maintenanceCalories);
+  $scope.macnutsRemaining = Macronutrients.forPhase($scope.calendar, lbm, maintenanceCalories);
 
   $scope.savePhase = function() {
     window.localStorage['phase'] = $scope.calendar.phase;
@@ -25,15 +23,10 @@ angular.module('man20-macnuttrk.controllers', [])
   $scope.saveIsWorkoutDay = function() {
     window.localStorage['isWorkoutDay'] = $scope.calendar.isWorkoutDay;
   };
-  
-  var stats = User.loadStats();
-  var lbm = User.calculateLBM(stats);
-  var maintenanceCalories = User.maintenanceCalories(stats);
-  
-  $scope.macnuts = Macronutrients.forPhase($scope.calendar, lbm, maintenanceCalories);
 
-  $scope.macnutsRemaining = Macronutrients.forPhase($scope.calendar, lbm, maintenanceCalories);
-
+  $scope.phases = [ 1, 2, 3, 4];
+  $scope.weeks = [1, 2, 3, 4];
+  
   $ionicPopover.fromTemplateUrl('change-phase-popover.html', {
     scope: $scope,
   }).then(function(popover) {
